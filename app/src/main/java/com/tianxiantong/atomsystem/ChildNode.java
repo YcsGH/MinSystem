fe7ec4c65cddba2f76d083877e17392fff4de5d8
yangcs <1214099793@qq.com>
2021-08-04 15:29:20 +0800

    v1.0.1 commit

---
+++ app/src/main/java/com/tianxiantong/atomsystem/ChildNode.java
@@ -0,0 +1,12 @@
+package com.tianxiantong.atomsystem;
+
+/**
+ * test class
+ */
+public class ChildNode {
+    private final String name = "N1";
+
+    public String getName() {
+        return name;
+    }
+}
